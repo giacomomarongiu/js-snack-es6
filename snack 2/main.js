aa52f14195d3bf62629b131cba6fd0af333fe1f4
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-03-05 14:11:44 +0100

    snack 2 done

--- snack 2/main.js
+++ snack 2/main.js
@@ -1,17 +1,19 @@
 console.log("Hello Snack 2");
 
-
-
 /* A partire da un array di stringhe, crea un secondo array 
 formattando le stringhe del primo array in minuscolo e
- con l’iniziale maiuscola.
- */
+ con l’iniziale maiuscola.*/
 
+ //Creo un array di stringhe
  const myWords = ["pippo", "pLUTO", "PAperino", "PippoBAUDO", "PIPPOFRanco"];
 
+ //Creo un uovo array modificando con map gli elementi del precente array
  const myWordsFix = myWords.map(function (word) {
+    // Applico UpperCase al primo carattere + dico di applicare LowerCase a partire dal secondo
      return word[0].toUpperCase() + word.slice(1).toLowerCase();
  })
  
+ console.log("Il mio array inizale");
  console.log(myWords);
+ console.log("Il mio array con le stringhe corrette")
  console.log(myWordsFix);
