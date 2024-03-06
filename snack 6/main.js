1ed296b5dbff517551553e0a64508350390019f7
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-03-06 16:00:38 +0100

    BONUS Done + comments + some Bootstrap

--- snack 6/main.js
+++ snack 6/main.js
@@ -46,6 +46,7 @@ let footballTeams = [
 //console.log(footballTeams);
 
 
+//FUNZIONI
 //Creo una funzione che mi genera numeri random da 0 a 50
 function randomFouls() {
     let x = Math.floor(Math.random() * 50);
@@ -65,18 +66,19 @@ function randomPoints() {
 
 
 
-//Utilizzo Map sul mio array per modificare il valori con numeri random
+//Utilizzo Map sul mio array per modificare il valori con i numeri random
 footballTeams.map(team => {
     //Utilizzo le mie funzioni qui dentro
     return team.puntifatti = randomPoints(), team.fallisubiti = randomFouls();
 })
+//Lo stampo
 console.log("Ecco l'array con le squadre e i valori casuali");
 console.log(footballTeams);
 
 
 
 //Creo un nuovo array vuoto 
-let footballTeamsNoFouls=[];
+let footballTeamsNoFouls = [];
 
 //Usando la destrutturazione lo vado a riempire solo con nom e fallisubiti
 footballTeams.forEach(team => {
@@ -85,5 +87,70 @@ footballTeams.forEach(team => {
     //E' come se a ogni ciclo pushassi nel mio array vuoto solo le mie due proprietà
     footballTeamsNoFouls = [...footballTeamsNoFouls, { nome, fallisubiti }]
 });
+//Lo stampo
 console.log("Ecco l'array con i puntifatti rimossi");
 console.log(footballTeamsNoFouls);
+
+
+
+
+
+
+//BONUS
+//CREO ELEMENTI SUL DOM
+// Utilizzo una struttura Bootstrap
+const main = document.createElement('main');
+const container = document.createElement('div');
+const row = document.createElement('div');
+
+//Creo struttura nella mia DOM
+document.body.append(main);
+main.append(container);
+container.append(row);
+
+//Assegno struttuta Bootstrap aggiungendo classi ai miei elementi sul DOM
+container.classList.add('container');
+row.classList.add('row','g-3');
+
+//Creo una funzione per stampare gli array sul DOM
+/** Questa funzione serve per stampare sul DOM un array di oggetti
+ * che hanno come paramentri nome, punti fatti, falli subiti;
+ * 
+ * @param {array} teams 
+ */
+function letsDOM(teams) {
+    teams.forEach(object => {
+        let {nome,fallisubiti,puntifatti}=object;
+        let differentBg= "bg-primary";
+        const col = document.createElement('div');
+        const card = document.createElement('div');
+
+        //Valuto il caso del secondo array e gli cambio colore per distiguersi
+        if(puntifatti===undefined){
+            puntifatti = "";
+            differentBg ="bg-secondary"
+        }
+
+        //Aggiungo a ogni iterazione una colonna
+        row.append(col);
+        col.classList.add('col-4','p-1');
+
+        //Aggiungo a ogni iterazione una card
+        col.append(card);
+        card.classList.add('card');
+
+        const markup = `
+    <div class="card-body ${differentBg}">
+    <h5 class="card-title">${nome}</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">Punti fatti:${puntifatti}</h6>
+    <p class="card-text">Falli subiti: ${fallisubiti}</p>
+     </div>`
+
+        card.innerHTML= markup;
+    })
+}
+
+
+//Chiamo la funzione per entrambi gli array
+letsDOM(footballTeams);
+letsDOM(footballTeamsNoFouls);
